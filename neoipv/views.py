220b8cf455901bcce21b73e79743625935cd2902
JUANCAMS23 <90731213+JUANCAMS23@users.noreply.github.com>
2023-11-04 11:06:40 -0500

    Nuevo proyecto

---
+++ neoipv/views.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template import loader
+
+def bienvenida(request):
+	return HttpResponse("Bienvenido a esta pagina")
+
+def bienvenidaRojo(request): # Pasamos un objeto de tipo request como primer argumento.
+	return HttpResponse("<p style='color: red;'>Bienvenido o bienvenida a este curso de Django. ;)</p>")
+
+def contenidoHTML(request):
+	plantillaExterna = open("C:/neoipv/neoipv/plantillas/plantilla.html")
+	template = Template(plantillaExterna.read())
+	plantillaExterna.close()
+	contexto = Context()
+	documento = template.render(contexto)
+	return HttpResponse(documento)
+
+def contenidoHTMLCargadores(request):
+	plantillaExterna = loader.get_template('plantilla2.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
+
+def PlantillaHija(request):
+	plantillaExterna = loader.get_template('PlantillaHija.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
+
+def PlantillaHija2(request):
+	plantillaExterna = loader.get_template('PlantillaHija2.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
+
+def Home(request):
+	plantillaExterna = loader.get_template('Home.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
+
+def AcercaDeNosotros(request):
+	plantillaExterna = loader.get_template('AcercaDeNosotros.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
+
+def ChatBoxInteractivo(request):
+	plantillaExterna = loader.get_template('ChatBoxInteractivo.html')
+	documento = plantillaExterna.render()
+	return HttpResponse(documento)
