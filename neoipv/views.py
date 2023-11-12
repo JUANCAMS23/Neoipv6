44ef689ef3cfa02ed022ad1d3d4b2cba9875924b
JUANCAMS23 <90731213+JUANCAMS23@users.noreply.github.com>
2023-11-12 16:30:27 -0500

    Update views.py

--- neoipv/views.py
+++ neoipv/views.py
@@ -3,19 +3,8 @@ from django.shortcuts import render
 from django.template import Template, Context
 from django.template import loader
 
-def bienvenida(request):
-	return HttpResponse("Bienvenido a esta pagina")
-
-def bienvenidaRojo(request): # Pasamos un objeto de tipo request como primer argumento.
-	return HttpResponse("<p style='color: red;'>Bienvenido o bienvenida a este curso de Django. ;)</p>")
-
 def contenidoHTML(request):
 	return render(request, 'Home.html')
-
-def contenidoHTMLCargadores(request):
-	plantillaExterna = loader.get_template('plantilla2.html')
-	documento = plantillaExterna.render()
-	return HttpResponse(documento)
 
 def PlantillaHija(request):
 	plantillaExterna = loader.get_template('PlantillaHija.html')
@@ -29,11 +18,6 @@ def PlantillaHija2(request):
 
 def Home(request):
 	plantillaExterna = loader.get_template('Home.html')
-	documento = plantillaExterna.render()
-	return HttpResponse(documento)
-
-def AcercaDeNosotros(request):
-	plantillaExterna = loader.get_template('AcercaDeNosotros.html')
 	documento = plantillaExterna.render()
 	return HttpResponse(documento)
 
